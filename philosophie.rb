51eff73a63d2199275690318f8460c46bbb9a0d9
Laucsonium <lucas.massoni51@gmail.com>
2017-07-14 16:40:26 +0200

    philosophie in progress

--- philosophie.rb
+++ philosophie.rb
@@ -3,5 +3,12 @@ require 'nokogiri'
 require 'open-uri'
 
 
-page="https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
-page=Nokogiri::HTML(open(page))
+base_page_url="https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
+
+while base_page_url!="https://fr.wikipedia.org/wiki/Philosophie"
+	page=Nokogiri::HTML(open(base_page_url))
+	first_link=page.xpath("//*[@id='mw-content-text']/div/p[1]/a[1]")
+	base_page_url=first_link[0]["href"]
+	puts base_page_url
+
+end
