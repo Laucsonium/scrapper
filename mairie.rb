a47d93efcca52ce5fe8972651b25c35aa023f74b
Laucsonium <lucas.massoni51@gmail.com>
2017-07-14 13:16:23 +0200

    ça marche bordel

--- mairie.rb
+++ mairie.rb
@@ -3,15 +3,26 @@ require 'nokogiri'
 require 'open-uri'
 
 
-def get_the_email_of_a_townhal_from_its_webpage (ville)
-	base_page_url="http://annuaire-des-mairies.com/95/"
-	page=Nokogiri::HTML(open("#{base_page_url}#{ville}.html"))
-	mail=page.xpath("//tr/td/p[@class='Style22']")
+def get_the_email_of_a_townhal_from_its_webpage (city_link)
+	base_page_url="http://annuaire-des-mairies.com"
+	page=Nokogiri::HTML(open("#{base_page_url}#{city_link}"))
+	mail=page.xpath('//table/tr[3]/td/table/tr[1]/td[1]/table[4]/tr[2]/td/table/tr[4]/td[2]/p/font')
 	mail.each do |x|
-	puts x.text if x.text.match("mairie.+")
+	puts x.text
 end
 
 end
 
+def get_all_urls_of_val_doise_townhalls ()
+	base_page_url="http://annuaire-des-mairies.com/val-d-oise.html"
+	page=Nokogiri::HTML(open("#{base_page_url}"))
+	links=page.xpath("//a[@class='lientxt']")
+	links.each do |link|
+		city_link=link["href"].sub! /^\./, ""
+		get_the_email_of_a_townhal_from_its_webpage(city_link)
+	end
+end
 
-get_the_email_of_a_townhal_from_its_webpage("ableiges")
+get_all_urls_of_val_doise_townhalls
+
+
