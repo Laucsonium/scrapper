e74bbfb16c59235b862e17603484dbc9626ca25b
Laucsonium <lucas.massoni51@gmail.com>
2017-07-14 12:39:01 +0200

    fonctionne pour une mairie

--- mairie.rb
+++ mairie.rb
@@ -6,9 +6,9 @@ require 'open-uri'
 def get_the_email_of_a_townhal_from_its_webpage (ville)
 	base_page_url="http://annuaire-des-mairies.com/95/"
 	page=Nokogiri::HTML(open("#{base_page_url}#{ville}.html"))
-	mail=page.css(" table tbody tr td.style27 p.Style22 font")
+	mail=page.xpath("//tr/td/p[@class='Style22']")
 	mail.each do |x|
-	puts x.text
+	puts x.text if x.text.match("mairie.+")
 end
 
 end
