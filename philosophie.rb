911a8a8c62f79dad4b6fb99c6fcede8ea4392d84
Laucsonium <lucas.massoni51@gmail.com>
2017-07-16 15:31:24 +0200

    philosophie fonctionne mais toutes les exceptions a faire encore

--- philosophie.rb
+++ philosophie.rb
@@ -6,9 +6,16 @@ require 'open-uri'
 base_page_url="https://fr.wikipedia.org/wiki/Sp%C3%A9cial:Page_au_hasard"
 
 while base_page_url!="https://fr.wikipedia.org/wiki/Philosophie"
+	
 	page=Nokogiri::HTML(open(base_page_url))
+	p
+
 	first_link=page.xpath("//*[@id='mw-content-text']/div/p[1]/a[1]")
-	base_page_url=first_link[0]["href"]
+
+
+	page_url=first_link[0]["href"]
+	base_page_url="https://fr.wikipedia.org#{page_url}"
+
 	puts base_page_url
 
 end
